8e33a348292249d7d16fb496f853f30fc54e8247
Karley Webster <80624137+kwebster77@users.noreply.github.com>
2023-03-09 23:04:27 +0000

    saturday session

--- mandatory/1-fix-functions.js
+++ mandatory/1-fix-functions.js
@@ -10,10 +10,10 @@
 
 */
 
-function getMood() {
-  let isHappy = true;
+function getMood(happy) {
+  // let isHappy = true;
 
-  if (isHappy) {
+  if (happy) {
     return "I am happy";
   } else {
     return "I am not happy";
@@ -21,9 +21,9 @@ function getMood() {
 }
 
 function greaterThan10(num) {
-  let isBigEnough;
+  // let isBigEnough;
 
-  if (isBigEnough) {
+  if (num > 10) {
     return "num is greater than 10";
   } else {
     return "num is not big enough";
