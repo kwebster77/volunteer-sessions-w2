dc56f8ce3f7743467764832ba7864ba41595cd79
Karley Webster <80624137+kwebster77@users.noreply.github.com>
2023-03-09 22:13:48 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,63 @@
+# Coursework
+
+Like learning a musical instrument, programming requires daily practice.
+
+The exercises are split into two folders: `mandatory` and `extra`. All homework in the `mandatory` section **must** be completed for homework by the following lesson.
+
+The `extra` folder contains exercises that you can complete to challenge yourself, but are not required for the following lesson.
+
+If you think you need to do more practice with the basics, then you can find some more exercises listed in `resources.md`. These exercises are not mandatory and won't be assigned as part of your coursework for the week.
+
+## Setting up your code editor
+
+
+There are some tools that will help you to write code. One of these, [Prettier](https://prettier.io/), formats your code, making it easier for you and others to read.
+
+### 1. Install prettier
+
+- In Visual Studio open the extensions panel (see https://code.visualstudio.com/docs/editor/extension-gallery#_browse-and-install-extensions)
+- Search for `Prettier - Code formatter`
+- Click install on the top result
+
+### 2. Enable formatting on save
+
+- In Visual Studio open the settings file (see https://code.visualstudio.com/docs/getstarted/settings#_creating-user-and-workspace-settings)
+- Search for `editor format`
+- Set `editor.formatOnSave` and `editor.formatOnPaste` to true
+
+
+## Running the code/tests
+
+The files for the mandatory/extra exercises are intended to be run as jest tests. 
+
+- Once you have cloned the repository, run `npm install` once in the terminal to install jest (and any necessary dependencies).
+- To run the tests for all mandatory/extra exercises, run `npm test`
+- To run only the tests for the mandatory exercises, run `npm test -- --selectProjects mandatory`
+- To run only the tests for the extra exercises, run `npm test -- --selectProjects extra`
+- To run a single exercise/test (for example `mandatory/1-writer.js`), run `npm test -- --testPathPattern mandatory/1-writer.js` (Remember, you can use tab-completion to get files relative to the current directory, so m`Tab ↹`/1-`Tab ↹` will autocomplete get you the test file starting with 1-)
+
+For more information about tests, look here:
+
+https://syllabus.codeyourfuture.io/guides/intro-to-tests
+
+Try out variant way of running tests:
+
+- `npm test` -> run all mandatory and extra tests
+- `npm test -- --selectProjects mandatory` -> run only mandatory tests
+- `npm test -- --testPathPattern mandatory/1-syntax-errors.js` -> run single test
+
+## Solutions
+
+The solutions for this coursework can be found here:
+
+https://github.com/CodeYourFuture/JavaScript-Core-1-Coursework-Week2-Solution
+
+This is a **private** repository. Please request access from your Teachers, Buddy or City Coordinator after the start of your next lesson.
+
+## Instructions for submission
+
+For your homework, we'll be using tests to check your answers. The tests will assert that your functions behave in the correct way. A **failing test** ❌ indicates that a function _is not_ behaving correctly; a **passing test** ✅ indicates that your function is behaving correctly.
+
+1. Complete the challenges in each file and save it once you're happy with your changes
+2. Run the script to check the results against the tests - all tests should read PASSED if you completed the challenges correctly. If a test reads FAILED, find the associated test to identify which function failed and fix it.
+3. Raise a PR once you're happy with the quality of your code
---
+++ mandatory/2-function-creation.js
@@ -0,0 +1,160 @@
+/*
+  Complete the function to return a boolean describing whether a user is acceptable. 
+  To be acceptable, two conditions must be fulfilled
+    1. the user should be 18 or older
+    2. the user must be logged in
+*/
+function isAcceptableUser(userAge, isLoggedIn) {}
+
+/*
+  Complete the function to apply discount percent based on how much is totalPrice in user cart.
+    - When the total price is greater than 200, a 10% discount should be applied
+    - When the total price is less than 200, a 5% discount should be applied
+
+  The function should return the new price to be paid (e.g. if the totalPrice is 150, a 5% discount
+    is applieds and 142.5 should be returned)
+*/
+
+function applyDiscount(totalPrice) {}
+
+/*
+  Complete the function to print to the console the odd numbers between 1 and limit (use a while loop):
+  */
+function printOddNumbers(limit) {}
+
+/*
+  Complete the buyTwoGetTheCheapestFree function: if user buys two items, the cheapest item will be free!
+  The function should return the price to be paid once the discount is applied.
+*/
+function buyTwoGetTheCheapestFree(price1, price2) {}
+
+/*
+  Complete the function to determine if it is suitable for a person to register based on their age!
+  - if the person is 12 or younger it should return "You Are Too Young To Register"
+  - if the person is older than 12 and younger than 90 it should return "You Can Register"
+  - if the person is 90 or older it should return "You Don't Need To Register"
+*/
+function canRegister(age) {}
+
+/*
+  Complete the function so that it prints out to the console numbers in reverse order starting at
+  number and going down to 1 (e.g. if number was 3, it would print:
+    3
+    2
+    1
+  )
+*/
+
+function countReverse(number) {}
+
+/* ======= TESTS - DO NOT MODIFY ===== */
+
+describe("isAcceptableUser", () => {
+  test("returns true if over 18 and logged in", () => {
+    expect(isAcceptableUser(21, true)).toEqual(true);
+  });
+
+  test("returns true if 18 and logged in", () => {
+    expect(isAcceptableUser(18, true)).toEqual(true);
+  });
+
+  test("returns false if under 18 and logged in", () => {
+    expect(isAcceptableUser(17, true)).toEqual(false);
+  });
+
+  test("returns false if over 18 and not logged in", () => {
+    expect(isAcceptableUser(21, false)).toEqual(false);
+  });
+});
+
+describe("applyDiscount", () => {
+  test("returns price with 5% discount", () => {
+    expect(applyDiscount(120)).toEqual(114);
+  });
+
+  test("returns price with 10% discount", () => {
+    expect(applyDiscount(280)).toEqual(252);
+  });
+});
+
+describe("printOddNumbers", () => {
+  test("printOddNumbers function prints odd numbers between 1 and 2", () => {
+    const consoleLogSpy = jest.spyOn(console, "log");
+    printOddNumbers(2);
+
+    expect(consoleLogSpy).toBeCalledWith(1);
+    consoleLogSpy.mockRestore();
+  });
+
+  test("printOddNumbers function prints odd numbers between 1 and 10", () => {
+    const consoleLogSpy = jest.spyOn(console, "log");
+
+    printOddNumbers(10);
+    expect(consoleLogSpy).toHaveBeenNthCalledWith(1,1);
+    expect(consoleLogSpy).toHaveBeenNthCalledWith(2,3);
+    expect(consoleLogSpy).toHaveBeenNthCalledWith(3,5);
+    expect(consoleLogSpy).toHaveBeenNthCalledWith(4,7);
+    expect(consoleLogSpy).toHaveBeenNthCalledWith(5,9);
+
+    consoleLogSpy.mockRestore();
+  });
+});
+
+describe("buyTwoGetTheCheapestFree", () => {
+  test("buyTwoGetTheCheapestFree function returns first price when it is largest", () => {
+    expect(buyTwoGetTheCheapestFree(700, 500)).toEqual(700);
+  });
+
+  test("buyTwoGetTheCheapestFree function returns second price when it is largest", () => {
+    expect(buyTwoGetTheCheapestFree(500, 700)).toEqual(700);
+  });
+});
+
+describe("canRegister", () => {
+  test("returns in case of a person of age 7", () => {
+    expect(canRegister(7)).toEqual("You Are Too Young To Register");
+  });
+
+  test("returns in case of a person of age 12", () => {
+    expect(canRegister(12)).toEqual("You Are Too Young To Register");
+  });
+
+  test("returns in case of a person of age 13", () => {
+    expect(canRegister(13)).toEqual("You Can Register");
+  });
+
+  test("returns in case of a person of age 89", () => {
+    expect(canRegister(89)).toEqual("You Can Register");
+  });
+
+  test("returns in case of a person of age 90", () => {
+    expect(canRegister(90)).toEqual("You Don't Need To Register");
+  });
+
+  test("returns in case of a person of age 112", () => {
+    expect(canRegister(112)).toEqual("You Don't Need To Register");
+  });
+});
+
+describe("countReverse", () => {
+  test("countReverse function logs values from 7 to 1", () => {
+    const consoleLogSpy = jest.spyOn(console, "log");
+
+    countReverse(7);
+    expect(consoleLogSpy).toHaveBeenNthCalledWith(1,7);
+    expect(consoleLogSpy).toHaveBeenNthCalledWith(2,6);
+    expect(consoleLogSpy).toHaveBeenNthCalledWith(3,5);
+    expect(consoleLogSpy).toHaveBeenNthCalledWith(4,4);
+    expect(consoleLogSpy).toHaveBeenNthCalledWith(5,3);
+    expect(consoleLogSpy).toHaveBeenNthCalledWith(6,2);
+    expect(consoleLogSpy).toHaveBeenNthCalledWith(7,1);
+
+    consoleLogSpy.mockRestore();
+  });
+});
+
+/*
+CHECK OUT solutions.md FOR MORE INFO ON OUR SOLUTION
+*/
+
+/* ======= TESTS - DO NOT MODIFY ===== */
